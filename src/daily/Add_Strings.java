5815968c02020dfbe54515122248a1676815987a
HeyWeCome <heywecome@163.com>
2020-08-03 09:09:14 +0800

    字符串相加

---
+++ src/daily/Add_Strings.java
@@ -0,0 +1,36 @@
+package daily;
+
+/**
+ * @description: 给定两个字符串形式的非负整数 num1 和num2 ，计算它们的和。
+ * 注意：
+ * num1 和num2 的长度都小于 5100.
+ * num1 和num2 都只包含数字 0-9.
+ * num1 和num2 都不包含任何前导零。
+ * 你不能使用任何內建 BigInteger 库， 也不能直接将输入的字符串转换为整数形式。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/add-strings
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author: HeyWeCome
+ * @createDate: 2020/8/3 8:46
+ */
+
+public class Add_Strings {
+    // 本质上，解题思路就是我们在纸上书写的加减法，只不过是用代码的形式展示出来。
+    public String addStrings(String num1, String num2) {
+        int i = num1.length() - 1, j = num2.length() - 1, add = 0;
+        StringBuffer ans = new StringBuffer();
+        while (i >= 0 || j >= 0 || add != 0) {
+            int x = i >= 0 ? num1.charAt(i) - '0' : 0;
+            int y = j >= 0 ? num2.charAt(j) - '0' : 0;
+            int result = x + y + add;
+            ans.append(result % 10);
+            add = result / 10;
+            i--;
+            j--;
+        }
+        // 计算完以后的答案需要翻转过来
+        ans.reverse();
+        return ans.toString();
+    }
+}
