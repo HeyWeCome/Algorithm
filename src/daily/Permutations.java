c93c10788e8a6fcfea28df2bdd68f7949eeb1ae8
HeyWeCome <heywecome@163.com>
2020-09-28 09:01:03 +0800

    旋转链表完成

---
+++ src/daily/Permutations.java
@@ -0,0 +1,18 @@
+package daily;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @description: 给定一个 没有重复 数字的序列，返回其所有可能的全排列。
+ * @author: HeyWeCome
+ * @createDate: 2020/9/18 20:02
+ * @version: 1.0
+ */
+public class Permutations {
+
+//    public List<List<Integer>> permute(int[] nums) {
+//        List<List<Integer>> res = new ArrayList<>();
+//        List<Integer> list = new ArrayList<>();
+//    }
+}
