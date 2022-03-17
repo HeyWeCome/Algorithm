24c12923045af8a167a5f701372da7e526c279c3
heywecome <heywecome@163.com>
2022-03-17 21:08:49 +0800

    leetcode每日一题720

---
+++ daily_question/720.词典中最长的单词.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string[]} words
+ * @return {string}
+ */
+ words = ["t","ti","tig","tige","tiger","e","en","eng","engl","engli","englis","english","h","hi","his","hist","histo","histor","history"]
+ // words = ["d","do","dog","p","pe","pen","peng","pengu","pengui","penguin","e","el","ele","elep","eleph","elepha","elephan","elephant"]
+ words = ["yo","ew","fc","zrc","yodn","fcm","qm","qmo","fcmz","z","ewq","yod","ewqz","y"]
+ 
+ var longestWord = function(words) {
+   words = words.sort();
+   let set = new Set();
+   let res = "";
+ 
+   for (const item of words) {
+     if (item.length == 1 || set.has(item.substring(0, item.length - 1))) {
+       res = item.length > res.length ? item : res;
+       set.add(item);
+     }
+   }
+ 
+   return res;
+ };
+ 
+ console.log(longestWord(words));
--- test.js
+++ test.js
@@ -1,13 +1,24 @@
-let aa = {
-	age: 18,
-	name: 'aaa',
-	address: {
-		city: 'shanghai'
+/**
+ * @param {string[]} words
+ * @return {string}
+ */
+words = ["t","ti","tig","tige","tiger","e","en","eng","engl","engli","englis","english","h","hi","his","hist","histo","histor","history"]
+// words = ["d","do","dog","p","pe","pen","peng","pengu","pengui","penguin","e","el","ele","elep","eleph","elepha","elephan","elephant"]
+words = ["yo","ew","fc","zrc","yodn","fcm","qm","qmo","fcmz","z","ewq","yod","ewqz","y"]
+
+var longestWord = function(words) {
+	words = words.sort();
+	let set = new Set();
+	let res = "";
+
+	for (const item of words) {
+		if (item.length == 1 || set.has(item.substring(0, item.length - 1))) {
+			res = item.length > res.length ? item : res;
+			set.add(item);
+		}
 	}
-}
 
-let bb = {...aa};
-bb.address.city = 'shenzhen';
-bb.name = 'kang'
+	return res;
+};
 
-console.log(aa);  // shenzhen
+console.log(longestWord(words));
