d6d0d0b9a50e1db29f90b839575f6eb9679c7d86
heywecome <heywecome@163.com>
2022-03-21 16:02:58 +0800

    二叉树的所有路径

--- tree/257.二叉树的所有路径.js
+++ tree/257.二叉树的所有路径.js
@@ -11,8 +11,25 @@
  * @return {string[]}
  */
 var binaryTreePaths = function(root) {
-  let queue = [];
-  if (root === null) return queue;
-
+  // 递归遍历+递归三部曲
+  let res = [];
   
+  // 1. 确认递归函数 函数参数
+  const getPath = (node, curPath) => {
+    // 2.确认终止条件
+    if (node.left === null && node.right === null) {
+      // 叶子节点的值后面不需要跟 '->'
+      curPath += node.val;
+      res.push(curPath);
+      return;
+    }
+
+    // 3.确定单层递归逻辑
+    curPath += node.val+'->';
+    if (node.left) getPath(node.left, curPath);
+    if (node.right) getPath(node.right, curPath);
+  }
+
+  getPath(root, '');
+  return res;
 };
